38dc482258563fe5c41a28e680ddbd3e0f4a3a46
ArpitSingh73 <arpitsingh73073@gmail.com>
2023-12-01 17:40:33 +0530

    Some other UI changes

--- app.py
+++ app.py
@@ -1,36 +1,29 @@
 import streamlit as st
 from chat import get_response
 
-st.title("Coffee shop Chatbot")
-# st.header("Coffee shop Chatbot")
-# st.write("Luna")
-st.divider()
-temp = st.chat_message("human")
-# static_ans = get_response("Hi")
-temp.write("Hi there, I am Luna. How can I help you?")
 
-input = st.chat_input("")
+var = st.sidebar.radio("Navigation", ["Home", "About"])
 
-# input = st.text_input(label="You :-")
-count = 0
+if var == "Home":
+    st.title("Coffee Shop Chatbot")
+    st.divider()
+    temp = st.chat_message("human")
+    temp.write("Hi there, I am Luna. How can I help you?")
+    input = st.chat_input("")
 
-
-while True:
-    if input:
-        temp = st.chat_message("assistant")
-        temp.write(get_response(input))
-        # st.success(get_response(input))
-        count = count + 1
-        input = ""
-        # input = st.text_input(label="You", key=count)
+    while True:
+        if input:
+            temp = st.chat_message("assistant")
+            temp.write(get_response(input))
+            input = ""
 
 
-# count = 0
-# input = st.chat_input(placeholder="You")
-# while True:
-#     if input != None:
-#         temp = st.chat_message("assistant")
-#         temp.write(get_response(input))
-#         input = None
-#         count = count + 1
-#         input = st.chat_input(placeholder="You", key=count)
+elif var == "About":
+    st.subheader(
+        "Hi there, This is a simple Chatbot, developed for Coffee a Shop. I made this bot to implement my learning of Artificial Neural Networks(ANN). I used PyTorch as framework for model training & the data was manually created. It can answer questions about -"
+    )
+
+    st.text("Bot itself.")
+    st.text("Service it offers.")
+    st.text("Methods of payment.")
+    st.text("Delivery info.")
